e1312006889a902087bc49f6640a86907e73f1d7
Greg Vetkin <gregoryvetkin@gmail.com>
2023-03-02 12:35:32 +0300

    Added solution for "14. Longest Common Prefix"
    Much easier and much faster

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -26,8 +26,25 @@ class Solution:
         return prefix
 
 
+    def longestCommonPrefix_goodsolution(self, strs: List[str]) -> str:
+        first_word, last_word = min(strs), max(strs)  # alphabetical sorting!
+        prefix = ''
+        for ind in range(min(len(first_word), len(last_word))):
+            if first_word[ind] != last_word[ind]:
+                break
+            prefix += first_word[ind]
+
+        return prefix
+
+
 if __name__ == '__main__':
+
     print([Solution().longestCommonPrefix(["dog", "racecar", "car"])])
     print([Solution().longestCommonPrefix(["flower", "flow", "flight"])])
     print([Solution().longestCommonPrefix(["", "flower", "flow", "flight"])])
     print([Solution().longestCommonPrefix(["aaa", "aa", "aaa"])])
+
+    print([Solution().longestCommonPrefix_goodsolution(["dog", "racecar", "car"])])
+    print([Solution().longestCommonPrefix_goodsolution(["flower", "flow", "flight"])])
+    print([Solution().longestCommonPrefix_goodsolution(["", "flower", "flow", "flight"])])
+    print([Solution().longestCommonPrefix_goodsolution(["aaa", "aa", "aaa"])])
