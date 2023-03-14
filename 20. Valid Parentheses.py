fcf7bac1f950c918b6e4cbb237f28eea46eb1854
Greg Vetkin <gregoryvetkin@gmail.com>
2023-03-14 11:14:58 +0300

    Solution: "20. Valid Parentheses"
    Added solution.
    Based on an example: https://leetcode.com/problems/valid-parentheses/solutions/2411675/very-easy-100-fully-explained-c-java-python-js-python3/

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,13 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        if s.count('(') != s.count(')') or s.count('[') != s.count(']') or s.count('{') != s.count('}'):
-            return False
+        pair = {'(': ')',
+                '{': '}',
+                '[': ']'}
+        stack = []
 
-        open = ('(', '[', '{')
-        close = (')', ']', '}')
+        for symbol in s:
 
+            if symbol in '([{':
+                stack.append(symbol)
+            elif len(stack) == 0 or symbol != pair[stack.pop()]:
+                return False
+
+        return len(stack) == 0
 
 
 if __name__ == '__main__':
-    print(Solution().isValid("[()]"))
+    print(Solution().isValid("{[][][]}"))
+    print(Solution().isValid("{[(][][]}"))
+    print(Solution().isValid("(]"))
